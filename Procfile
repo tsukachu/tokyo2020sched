22b006a9695fb474911bb1a1546f62e32172c3df
tsukachu <concon.ch.1543@gmail.com>
2019-06-07 19:34:57 +0900

    Heroku 向けの設定を追加

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: bin/tokyo2020-sch-api
--- main.go
+++ main.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+
 	e := echo.New()
+
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		return c.String(http.StatusOK, "hello, world")
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+
+	e.Logger.Fatal(e.Start(":" + port))
 }
