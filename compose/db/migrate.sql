ff611cdeabe3c69e35282558b3678e23ca1fba94
tsukachu <concon.ch.1543@gmail.com>
2019-06-09 17:19:58 +0900

    場所とスケジュールのマスターデータを追加

--- compose/db/migrate.sql
+++ compose/db/migrate.sql
@@ -29,3 +29,33 @@ CREATE TRIGGER updated_at
   BEFORE UPDATE ON classification
   FOR EACH ROW
   EXECUTE PROCEDURE set_updated_at();
+
+CREATE TABLE place (
+  id int4 PRIMARY KEY,
+  name varchar(32) NOT NULL,
+  created_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TRIGGER updated_at
+  BEFORE UPDATE ON place
+  FOR EACH ROW
+  EXECUTE PROCEDURE set_updated_at();
+
+CREATE TABLE olympic_schedule (
+  id int4 PRIMARY KEY,
+  competition_id int4 references competition(id),
+  classification_id int4 references classification(id),
+  title varchar(24) NOT NULL,
+  "begin" timestamptz NOT NULL,
+  "end" timestamptz NOT NULL,
+  place_id int4 references place(id),
+  content varchar(40),
+  created_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TRIGGER updated_at
+  BEFORE UPDATE ON olympic_schedule
+  FOR EACH ROW
+  EXECUTE PROCEDURE set_updated_at();
--- migrate.sh
+++ migrate.sh
@@ -3,3 +3,5 @@
 psql -d tokyo2020_sch -h 127.0.0.1 -U user1 -f compose/db/migrate.sql
 psql -d tokyo2020_sch -h 127.0.0.1 -U user1 -c "\COPY competition (id, name) FROM 'compose/db/master/competitions.csv' CSV HEADER"
 psql -d tokyo2020_sch -h 127.0.0.1 -U user1 -c "\COPY classification (id, name, competition_id) FROM 'compose/db/master/classifications.csv' CSV HEADER"
+psql -d tokyo2020_sch -h 127.0.0.1 -U user1 -c "\COPY place (id, name) FROM 'compose/db/master/places.csv' CSV HEADER"
+psql -d tokyo2020_sch -h 127.0.0.1 -U user1 -c "\COPY olympic_schedule (id, competition_id, classification_id, title, \"begin\", \"end\", place_id, content) FROM 'compose/db/master/olympic_schedules.csv' CSV HEADER"
