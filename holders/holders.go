3c51d37560b0a86c5a6d5acb30280daa9a1c6ab1
tsukachu <concon.ch.1543@gmail.com>
2019-06-09 22:34:35 +0900

    struct を分割

---
+++ holders/holders.go
@@ -0,0 +1,26 @@
+package holders
+
+import "time"
+
+type Competition struct {
+	Id        int64     `db:"id"`
+	Name      string    `db:"name"`
+	CreatedAt time.Time `db:"created_at"`
+	UpdatedAt time.Time `db:"updated_at"`
+}
+
+type Classification struct {
+	Id            int64     `db:"id"`
+	Name          string    `db:"name"`
+	CompetitionId int64     `db:"competition_id"`
+	CreatedAt     time.Time `db:"created_at"`
+	UpdatedAt     time.Time `db:"updated_at"`
+}
+
+type ClassificationWithCompetition struct {
+	Id          int64
+	Name        string
+	Competition Competition
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
--- main.go
+++ main.go
@@ -12,41 +12,9 @@ import (
 	"github.com/labstack/echo/middleware"
 	_ "github.com/lib/pq"
 	"gopkg.in/gorp.v1"
+
+	"tokyo2020-sch-api/holders"
 )
-
-type Competition struct {
-	Id        int64     `db:"id"`
-	Name      string    `db:"name"`
-	CreatedAt time.Time `db:"created_at"`
-	UpdatedAt time.Time `db:"updated_at"`
-}
-
-type Classification struct {
-	Id            int64     `db:"id"`
-	Name          string    `db:"name"`
-	CompetitionId int64     `db:"competition_id"`
-	CreatedAt     time.Time `db:"created_at"`
-	UpdatedAt     time.Time `db:"updated_at"`
-}
-
-type JoinedClassification struct {
-	Id                   int64
-	Name                 string
-	CreatedAt            time.Time `db:"created_at"`
-	UpdatedAt            time.Time `db:"updated_at"`
-	CompetitionId        int64     `db:"co_id"`
-	CompetitionName      string    `db:"co_name"`
-	CompetitionCreatedAt time.Time `db:"co_created_at"`
-	CompetitionUpdatedAt time.Time `db:"co_updated_at"`
-}
-
-type ClassificationWithCompetition struct {
-	Id          int64
-	Name        string
-	Competition Competition
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
-}
 
 func initDb() *gorp.DbMap {
 	dsn := os.Getenv("DSN")
@@ -58,8 +26,8 @@ func initDb() *gorp.DbMap {
 
 	dbmap := &gorp.DbMap{Db: db, Dialect: gorp.PostgresDialect{}}
 
-	dbmap.AddTableWithName(Competition{}, "competitions").SetKeys(true, "Id")
-	dbmap.AddTableWithName(Classification{}, "classifications").SetKeys(true, "Id")
+	dbmap.AddTableWithName(holders.Competition{}, "competitions").SetKeys(true, "Id")
+	dbmap.AddTableWithName(holders.Classification{}, "classifications").SetKeys(true, "Id")
 
 	err = dbmap.CreateTablesIfNotExists()
 	if err != nil {
@@ -93,7 +61,7 @@ func main() {
 	})
 
 	e.GET("/competitions/", func(c echo.Context) error {
-		var competitions []Competition
+		var competitions []holders.Competition
 
 		_, err := dbmap.Select(&competitions, "SELECT * FROM competition")
 		if err != nil {
@@ -107,7 +75,7 @@ func main() {
 	e.GET("/competitions/:id/", func(c echo.Context) error {
 		id := c.Param("id")
 
-		var competition Competition
+		var competition holders.Competition
 		err := dbmap.SelectOne(&competition, "SELECT * FROM competition WHERE id = $1", id)
 		if err != nil {
 			if err == sql.ErrNoRows {
@@ -122,7 +90,16 @@ func main() {
 	})
 
 	e.GET("/classifications/", func(c echo.Context) error {
-		var classifications []JoinedClassification
+		var classifications []struct {
+			Id                   int64
+			Name                 string
+			CreatedAt            time.Time `db:"created_at"`
+			UpdatedAt            time.Time `db:"updated_at"`
+			CompetitionId        int64     `db:"co_id"`
+			CompetitionName      string    `db:"co_name"`
+			CompetitionCreatedAt time.Time `db:"co_created_at"`
+			CompetitionUpdatedAt time.Time `db:"co_updated_at"`
+		}
 
 		_, err := dbmap.Select(&classifications, "SELECT cl.id, cl.name, cl.created_at, cl.updated_at, co.id AS co_id, co.name AS co_name, co.created_at AS co_created_at, co.updated_at AS co_updated_at FROM classification AS cl INNER JOIN competition AS co ON (cl.competition_id = co.id)")
 		if err != nil {
@@ -130,12 +107,12 @@ func main() {
 			return c.String(http.StatusBadRequest, err.Error())
 		}
 
-		result := make([]ClassificationWithCompetition, len(classifications))
+		result := make([]holders.ClassificationWithCompetition, len(classifications))
 		for i, v := range classifications {
-			result[i] = ClassificationWithCompetition{
+			result[i] = holders.ClassificationWithCompetition{
 				Id:   v.Id,
 				Name: v.Name,
-				Competition: Competition{
+				Competition: holders.Competition{
 					Id:        v.CompetitionId,
 					Name:      v.CompetitionName,
 					CreatedAt: v.CreatedAt,
@@ -152,7 +129,16 @@ func main() {
 	e.GET("/classifications/:id/", func(c echo.Context) error {
 		id := c.Param("id")
 
-		var classification JoinedClassification
+		var classification struct {
+			Id                   int64
+			Name                 string
+			CreatedAt            time.Time `db:"created_at"`
+			UpdatedAt            time.Time `db:"updated_at"`
+			CompetitionId        int64     `db:"co_id"`
+			CompetitionName      string    `db:"co_name"`
+			CompetitionCreatedAt time.Time `db:"co_created_at"`
+			CompetitionUpdatedAt time.Time `db:"co_updated_at"`
+		}
 		err := dbmap.SelectOne(&classification, "SELECT cl.id, cl.name, cl.created_at, cl.updated_at, co.id AS co_id, co.name AS co_name, co.created_at AS co_created_at, co.updated_at AS co_updated_at FROM classification AS cl INNER JOIN competition AS co ON (cl.competition_id = co.id) WHERE cl.id = $1", id)
 		if err != nil {
 			if err == sql.ErrNoRows {
@@ -163,10 +149,10 @@ func main() {
 			return c.String(http.StatusBadRequest, err.Error())
 		}
 
-		return c.JSON(http.StatusOK, ClassificationWithCompetition{
+		return c.JSON(http.StatusOK, holders.ClassificationWithCompetition{
 			Id:   classification.Id,
 			Name: classification.Name,
-			Competition: Competition{
+			Competition: holders.Competition{
 				Id:        classification.CompetitionId,
 				Name:      classification.CompetitionName,
 				CreatedAt: classification.CreatedAt,
